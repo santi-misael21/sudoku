03114d562db4c5e2fff5e496b60c4f496782b88d
santi-misael21 <santiagoalamos2000@gmail.com>
2022-09-20 00:37:44 -0300

    modulado simplificado

--- src/components/4-Four.jsx
+++ src/components/4-Four.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { actualizarCubos, create } from '../functions/3-Creator.js';
 import '../styles/0-zero.css';
 import { qubs, update, cols, boxs, restore, poss, detectdifficulty, megasolution } from '../functions/2-Estructures.js';
 import { solution } from '../functions/0-Unrepeatable.js';
@@ -10,16 +9,16 @@ import { solution } from '../functions/0-Unrepeatable.js';
 
 
 
-export default function Four({unity, rows, loyalindex}){
+export default function Four({unity, rows, loyalindex, carga}){
     
-    console.log(unity, loyalindex) //rows = [  [  0,0,0,  0,0,0,  0,0,0  ]  ,  [...]  ]
+    // console.log(unity, loyalindex) //rows = [  [  0,0,0,  0,0,0,  0,0,0  ]  ,  [...]  ]
 
     if(loyalindex===9){
     }
 
     let [filas, setFilas]= useState(rows)
 
-    // create()
+    // // create()
     
     function changeEach(e){
         console.log("unity, value", unity, loyalindex, e.target.value)
@@ -50,40 +49,43 @@ export default function Four({unity, rows, loyalindex}){
         console.log(filas, 'cols: ', cols, 'boxs: ', boxs)
 
         //Etapa de repetidos
-        function findReps(entidad) {
-            var finder
-            for (let b= 0; b< 9; b++) {
-                finder= solution(entidad[b])
-                if (finder !== -1) return finder
-            }
-            return finder
-        }
-        console.log( "cols: ",
-            findReps(cols), 'rows: ',
-            findReps(rows), 'boxs: ',
-            findReps(boxs),
-        )
+        // function findReps(entidad) {
+        //     var finder
+        //     for (let b= 0; b< 9; b++) {
+        //         finder= solution(entidad[b])
+        //         if (finder !== -1) return finder
+        //     }
+        //     return finder
+        // }
+        // console.log( "cols: ",
+        //     findReps(cols), 'rows: ',
+        //     findReps(rows), 'boxs: ',
+        //     findReps(boxs),
+        // )
         // return setEach(val)
     }
 
-    if(unity!==0){
-        document.getElementsByTagName("input")[loyalindex].value= unity
-    }
-    if(unity===0){
-        document.getElementsByTagName("input")[loyalindex].value= ''
+
+    if (carga) {
+        if(unity!==0){
+            document.getElementsByTagName("input")[loyalindex].value= unity
+        }
+        if(unity===0){
+            document.getElementsByTagName("input")[loyalindex].value= ''
+        }
     }
 
     return (
-        <span >
-            <input 
-            // id={repeat ? 'rep':''} 
-            className=
-            // {(prop%9!== 0 && prop%3 === 0)?'border': (divis === 3 || divis === 6)? 'under':
-            {'unity'}
-            // // value= {each}
-            onChange= {e=>changeEach(e)}
-            // // name= {each}
-            />
-        </span>
+            <span >
+                <input 
+                // id={repeat ? 'rep':''} 
+                className=
+                // {(prop%9!== 0 && prop%3 === 0)?'border': (divis === 3 || divis === 6)? 'under':
+                {'unity'}
+                // // value= {each}
+                onChange= {e=>changeEach(e)}
+                // // name= {each}
+                />
+            </span>
     )
 }
--- src/functions/2-Estructures.js
+++ src/functions/2-Estructures.js
@@ -1,5 +1,7 @@
 import { solution } from "./0-Unrepeatable.js";
-// const {solution} = require('../functions/0-Unrepeatable.js');
+// const {solution} = require('../functions/0-Unrepeatable.js');// import { solution } from "./0-Unrepeatable"
+// import { boxs, cols, poss, qubs, resetSquare, rows, update, updateBox, updateCol, updateRow } from "./2-Estructures";
+// poss()
 
 let cols, boxs ,rows
 
@@ -150,13 +152,19 @@ function possBoxs(){
     }
     return boxs
 }
-
+// update()
 function poss(){
     for (let a= 0; a < 81; a++) {
         update(a)
     }
 }
 
+function possRandC(boxs){
+    possCols()
+    possRows()
+    boxs= possCols()
+    console.log(boxs)
+}
 
 function restore(){
     for (let a= 0; a < 81; a++) {
@@ -195,6 +203,183 @@ function detectdifficulty(qubs){
 function updateEveryCols(){ //Hace un bucle de 81 iteraciones para actualizar las cols respecto a los qubs
     for(let b=0;b<81;b++) updateCol(b) 
 }
+
+
+function updateQubsBy(what){
+    var q= 0
+    var qubes= []
+    for(let aa= 0; aa < 9; aa++){
+        for(let a= 0; a < 9; a++){
+            if(what==="rows") {
+                qubes.push(rows[aa][a])
+            }
+            if(what==="cols") {
+                qubes.push(cols[a][aa])
+            }
+            if(what==="boxs") {
+                var gross= Math.floor(q / 9) //indica el numero de fila
+                var index= Math.floor(gross / 3) //este da 0, 1 ó 2
+                var module= (q % 9)
+                let indexbox = (Math.floor(q/9)*3 + q%3) %9 //índice puntual -> 0 al 8
+                var foundbox= Math.floor(module / 3) + index * 3 //índice del box -> 0 al 8
+                qubes.push(boxs[foundbox][indexbox])
+                q++
+            }            
+        }
+    }
+    return qubes
+}
+
+
+
+function create3(){
+    var molino= 0
+    var ochouno= 0
+    let setRows= new Set()
+    let setBoxs= new Set()
+    let theSet= new Set() 
+    for (let aa= 0; aa < 9; aa++) {
+        for (let a= 0; a < 9; a++) {
+            molino++
+            let random= Math.ceil( Math.random() * 9 ) 
+            rows[aa][a]= random
+            // if (a > 0){
+                if ( solution( rows[aa] ) !== -1 ) {
+                    setRows.add(random)             
+                    if(setRows.size >= 9) {
+                        // rows[aa][a]= 0
+                        // console.log("set a 9 en rows",theSet)
+                        // console.log(setRows, aa, a, random, ochouno, 'rows == 9', rows[aa])     
+                        setBoxs= new Set()
+                        setRows= new Set()
+                        rows[aa]= [0,0,0,  0,0,0,  0,0,0]
+                        qubs= updateQubsBy("rows")
+                        for(let b=0;b<9;b++){
+                            updateBox((aa * 9) +b)
+                            updateCol((aa * 9) +b)
+                        }
+                        ochouno= aa* 9
+                        a= -1
+                        continue
+                    }
+                    // console.log(setRows, aa, a, random, ochouno, 'rows', rows[aa])     
+                    a= a-1
+                    continue
+                }
+            // }
+            cols[a][aa]= random
+            if (aa > 0){
+                if ( solution( cols[a] ) !== -1 ) {
+                    setRows.add(random)             
+                    if(setRows.size >= 9) {
+                        // rows[aa][a]= 0
+                        // console.log("set a 9 en rows",theSet)
+                        // console.log(setRows, aa, a, random, ochouno, 'rows == 9', rows[aa])     
+                        setBoxs= new Set()
+                        setRows= new Set()
+                        rows[aa]= [0,0,0,  0,0,0,  0,0,0]
+                        qubs= updateQubsBy("rows")
+                        for(let b=0;b<9;b++){
+                            updateBox((aa * 9) +b)
+                            updateCol((aa * 9) +b)
+                        }
+                        ochouno= aa* 9
+                        a= -1
+                        continue
+                    }
+                    // console.log(setRows, aa, a, random, ochouno, 'rows', rows[aa])     
+                    a= a-1
+                    continue
+                }
+            }
+                // console.log("pasé los continue")
+            // }
+
+            var q= ochouno
+            var gross= Math.floor(q / 9) //indica el numero de fila
+            var index= Math.floor(gross / 3) //este da 0, 1 ó 2
+            var module= (q % 9)
+            let indexbox = (Math.floor(q/9)*3 + q%3) %9 //índice puntual -> 0 al 8
+            var foundbox= Math.floor(module / 3) + index * 3 //índice del box -> 0 al 8
+            boxs[foundbox][indexbox]= random
+            
+            // if( indexbox > 0 ) {
+                if ( solution( boxs[foundbox] ) !== -1 ) {
+                    setRows.add(random)
+                    boxs[foundbox][indexbox]= 0                     
+                    // console.log(setRows, aa, a, random, ochouno)                    
+                    if(setRows.size >= 9) {
+                        setBoxs= new Set()
+                        setRows= new Set()
+                        rows[aa]= [0,0,0,  0,0,0,  0,0,0]
+                        qubs= updateQubsBy("rows")
+                        for(let b=0;b<9;b++){
+                            updateBox((aa * 9) +b)
+                            updateCol((aa * 9) +b)
+                        }
+                        ochouno= aa* 9
+                        a= -1
+                        // ochouno= aa * 9
+                        continue
+                    }
+                    a=a-1
+                    // ochouno--
+                    continue
+                }
+            // }
+            if(/*solution(cols[a ])=== -1 &&*/ solution(rows[aa])=== -1){
+                rows[aa][a]= random
+                // cols[a][aa]= random
+            }
+            ochouno++
+            setBoxs= new Set()
+            setRows= new Set()
+        }
+        theSet= new Set()
+    }
+    return {ok:"ok", molino}
+}
+
+function difficult(level) {
+    var coef= level *0.4 +5
+
+    var diff= Math.random() * 15 
+    var float= parseFloat(diff.toString().slice(0,3))
+
+    if(float > coef) {
+        return true //se busca un numero
+    }
+    return false //no se busca un carajo
+}
+
+function sudoqubs (level, entity){
+    var suerte
+    var array = []
+    for (let index = 0; index < 9; index++) { 
+        for (let ii = 0; ii < 9; ii++) {
+            suerte= difficult (level)
+            if (!suerte) {
+            entity[index][ii]= 0 
+            array.push(index)
+            }
+        }
+    }
+    return array.length
+}
+
+poss()
+// update()
+// possRandC()
+restore()
+let sudoku =create3()
+sudoqubs(5, rows)
+qubs= updateQubsBy("rows")
+updateEvery()
+
+
+
+// console.log(cols, rows, boxs, qubs)
+
 // module.exports={
 export {
     //Para testing
@@ -211,10 +396,14 @@ export {
     possRows,
     possBoxs,
     poss,
+    possRandC,
     restore,
     detectdifficulty,
     megasolution,
     //Para crear sudoku lógico:
+    create3,
+    updateQubsBy, 
+    sudoku
     //Pendiente en proceso
 }
 
